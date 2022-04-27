722329d6e73a6a40ce443019328e7c943cc9ca5a
RobPasMue <roberto.pastormuela@ansys.com>
2022-04-27 10:27:09 +0200

    Change to pyproject.toml file

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="pyansys-tools-report",
-    version="0.1.dev0",
-    description="Ansys tool for reporting your Python environment's package versions and hardware resources in a standardized way.",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-)

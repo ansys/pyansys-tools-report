bbb66cb14dd2e7afa8dc7af766e147cef912d9fc
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2022-05-30 12:45:08 +0200

    Implement workflows and unit testing (#7)
    * Update pyproject.toml to include coverage in tests
    
    * Add cov results folder to gitignore
    
    * Include GitHub Actions
    
    * Renaming + include release stage
    
    * Typo - invalid stage name
    
    * Include badges and rename stage
    
    * Update on badges
    
    * Remove unnecessary title
    
    * INcluding unit tests
    
    * Pre-commit check style
    
    * Additional test for stdout str
    
    * Define "name" for release stage
    
    * Trial - no version on action
    
    * Trial - using "latest"
    
    * Update .github/workflows/ci.yml
    
    Co-authored-by: Jorge Martínez <28702884+jorgepiloto@users.noreply.github.com>
    
    * Using updated actions
    
    * Changing name to workflow
    
    * Changing caching keys
    
    * Remove unsupported version of python
    
    * Missing version
    
    * Typo in keys for restoring cache
    
    * Define main python version var
    
    * Update .github/workflows/ci.yml
    
    Co-authored-by: Jorge Martínez <28702884+jorgepiloto@users.noreply.github.com>
    
    * Improving test comments
    
    Co-authored-by: Jorge Martínez <28702884+jorgepiloto@users.noreply.github.com>

---
+++ tests/test_report.py
@@ -0,0 +1,168 @@
+import multiprocessing
+import os
+import platform
+
+import ansys.tools.report as report
+
+
+def test_create_ansys_report_empty():
+    """Test the creation of a Report and its correct output."""
+
+    # Instantiate a "default" Report
+    rep = report.Report()
+
+    # Assert some of its information
+    #
+    # CPU Count
+    assert rep.cpu_count == multiprocessing.cpu_count()
+    # Architecture
+    assert rep.architecture == platform.architecture()[0]
+    # OS
+    assert rep.system == platform.system()
+    # Machine
+    assert rep.machine == platform.machine()
+
+    # Assert the output of the project info (the one we can control)
+    str_empty = """
+Ansys Environment Report
+-------------------------------------------------------------------------------
+
+
+Ansys Installation
+******************
+No Ansys installations provided
+
+
+Ansys Environment Variables
+***************************
+None"""
+
+    # Assert that the empty report is properly generated
+    assert rep.project_info() == str_empty
+
+
+def test_create_ansys_report_with_libs():
+    """Test the creation of a Report and its correct output
+    when imaginary Ansys libraries are provided."""
+
+    # Let us imagine some ansys libraries
+    my_ansys_libs = {
+        "MyLib1": "v1.2",
+        "MyLib2": "v1.3",
+    }
+
+    # Instantiate a Report object
+    rep = report.Report(ansys_libs=my_ansys_libs)
+
+    # Assert the output of the project info (the one we can control)
+    str_report = """
+Ansys Environment Report
+-------------------------------------------------------------------------------
+
+
+Ansys Installation
+******************
+Version   Location
+------------------
+MyLib1       v1.2
+MyLib2       v1.3
+
+
+Ansys Environment Variables
+***************************
+None"""
+
+    # Assert that the report is properly generated
+    assert rep.project_info() == str_report
+
+
+def test_create_ansys_report_with_vars():
+    """Test the creation of a Report and its correct output
+    when imaginary Ansys variables are provided."""
+
+    # Let us imagine some ansys variables
+    os.environ["MYVAR_1"] = "VAL_1"
+    os.environ["MYVAR_2"] = "VAL_2"
+    my_ansys_vars = ["MYVAR_1", "MYVAR_2"]
+
+    # Instantiate a Report object
+    rep = report.Report(ansys_vars=my_ansys_vars)
+
+    # Assert the output of the project info (the one we can control)
+    str_report = """
+Ansys Environment Report
+-------------------------------------------------------------------------------
+
+
+Ansys Installation
+******************
+No Ansys installations provided
+
+
+Ansys Environment Variables
+***************************
+MYVAR_1                        VAL_1
+MYVAR_2                        VAL_2"""
+
+    # Assert that the report is properly generated
+    assert rep.project_info() == str_report
+
+
+def test_create_ansys_report_with_libs_and_vars():
+    """Test the creation of a Report and its correct output
+    when imaginary Ansys libraries and variables are provided."""
+
+    # Let us imagine some ansys libraries
+    my_ansys_libs = {
+        "MyLib1": "v1.2",
+        "MyLib2": "v1.3",
+    }
+
+    # Let us imagine some ansys variables
+    os.environ["MYVAR_1"] = "VAL_1"
+    os.environ["MYVAR_2"] = "VAL_2"
+    my_ansys_vars = ["MYVAR_1", "MYVAR_2"]
+
+    # Instantiate a Report object
+    rep = report.Report(ansys_libs=my_ansys_libs, ansys_vars=my_ansys_vars)
+
+    # Assert the output of the project info (the one we can control)
+    str_report = """
+Ansys Environment Report
+-------------------------------------------------------------------------------
+
+
+Ansys Installation
+******************
+Version   Location
+------------------
+MyLib1       v1.2
+MyLib2       v1.3
+
+
+Ansys Environment Variables
+***************************
+MYVAR_1                        VAL_1
+MYVAR_2                        VAL_2"""
+
+    # Assert that the report is properly generated
+    assert rep.project_info() == str_report
+
+
+def test_create_ansys_repr():
+    """Test the creation of a Report and its correct output
+    when directly calling the object."""
+
+    # Let us start by creating a "default" Report
+    str_rep = report.Report().__repr__()
+
+    # Define the comparison strings
+    str_start = "-" * 79 + "\nPyAnsys Software and Environment Report"
+    str_end = """
+Ansys Environment Variables
+***************************
+None"""
+
+    # Validate the start and end of the report
+    assert str_rep.startswith(str_start)
+    assert str_rep.endswith(str_end)

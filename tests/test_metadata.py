4732e9e1d032551fec30f995522752a23f34887c
RobPasMue <roberto.pastormuela@ansys.com>
2022-06-14 10:21:59 +0200

    Bump version 0.1.0. to version 0.1.1

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -4,4 +4,4 @@ import ansys.tools.report as report
 def test_pkg_version():
     """Test the correct functioning of the version() method."""
 
-    assert report.version() == "0.1.0"
+    assert report.version() == "0.1.1"
